d4f2e5b653dd864a49445943e7adccfd44355c5d
Jowie Mankga <jnmankga333@gmail.com>
2023-04-06 11:47:06 +0200

    //Migrations

---
+++ Migrations/20230406094411_NameFixproducer.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eTicketss.Migrations
+{
+    public partial class NameFixproducer : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
